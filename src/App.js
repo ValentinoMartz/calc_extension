406d427bc2ab0e1cba199991d943f8cedb0d08de
ValentinoMartz <valentinomartinez55@gmail.com>
2022-11-05 23:01:22 -0300

    buttons

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex bg-slate-500">
+      <div className="flex">
+        <div className="">
+          <input type="text" placeholder="..." />
+          <button className="m-5">+</button>
+          <button className="m-5">-</button>
+          <button className="m-5">*</button>
+          <button className="m-5">/</button>
+          <button className="m-5">=</button>
+        </div>
+      </div>
     </div>
   );
 }
