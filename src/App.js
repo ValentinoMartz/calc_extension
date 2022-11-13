2d3764c7bc101faf51ddd87533ee8e1a252f7505
ValentinoMartz <valentinomartinez55@gmail.com>
2022-11-12 21:20:14 -0300

    Tailwind done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,103 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex bg-slate-500">
-      <div className="flex">
-        <div className="">
-          <input type="text" placeholder="..." />
-          <button className="m-5">+</button>
-          <button className="m-5">-</button>
-          <button className="m-5">*</button>
-          <button className="m-5">/</button>
-          <button className="m-5">=</button>
-        </div>
-      </div>
+    <div className=" bg-slate-500 w-[320px] mt-[100px] m-auto">
+      <input
+        type="text"
+        placeholder="0"
+        className="bg-[#e3e3e3] p-10 font-[35px] w-full h-[45px] border-[3px] border-black outline-none hover:border-[#356be0] hover:text-[#356be0]"
+      />
+      <input
+        type="button"
+        value="1"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="2"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="3"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="4"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="5"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="6"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="7"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="8"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="9"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="0"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="+"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="-"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="*"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="/"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="%"
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="."
+        className="bg-[#333] h-[80px] w-[80px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+
+      <input
+        type="button"
+        value="clear"
+        className="bg-[#333] h-[80px] w-[160px] text-white font-[30px] border-black border-[1px] float-left"
+      />
+      <input
+        type="button"
+        value="="
+        className="bg-[#333] h-[80px] w-[160px] text-white font-[30px] border-black border-[1px] float-left"
+      />
     </div>
   );
 }
